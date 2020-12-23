878d31a1a154a6ef3df1071e60bf96c9a6b05b9d
Jeremy <jeremy.dufeux@gmail.com>
2020-12-23 09:40:45 +0100

    Use AsyncListDiffer in recyclerView

--- app/src/main/java/com/openclassrooms/savemytrip/todolist/ItemAdapter.java
+++ app/src/main/java/com/openclassrooms/savemytrip/todolist/ItemAdapter.java
@@ -2,6 +2,8 @@ package com.openclassrooms.savemytrip.todolist;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.AsyncListDiffer;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,14 +23,14 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
         void onClickShareButton(int position);
     }
     private final Listener callback;
-    private RequestManager glide;
+    private final RequestManager glide;
 
-    private List<Item> items;
+    AsyncListDiffer<Item> differ;
 
     public ItemAdapter(Listener callback, RequestManager glide) {
         this.callback = callback;
         this.glide = glide;
-        items = new ArrayList<>();
+        differ = new AsyncListDiffer<>(this, new DifferCallback());
     }
 
     @NonNull
@@ -42,20 +44,30 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder itemViewHolder, int i) {
-        itemViewHolder.updateWithItem(items.get(i), callback, glide);
+        itemViewHolder.updateWithItem(differ.getCurrentList().get(i), callback, glide);
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        return differ.getCurrentList().size();
     }
 
     public Item getItem(int position){
-        return items.get(position);
+        return differ.getCurrentList().get(position);
     }
 
     public void updateData(List<Item> items){
-        this.items = items;
-        notifyDataSetChanged();
+        differ.submitList(items);
     }
+
+    public static class DifferCallback extends DiffUtil.ItemCallback<Item> {
+        public boolean areItemsTheSame(Item oldItem, Item newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        public boolean areContentsTheSame(Item oldItem, @NonNull Item newItem) {
+            return oldItem.getText().equals(newItem.getText());
+        }
+    }
+
 }
